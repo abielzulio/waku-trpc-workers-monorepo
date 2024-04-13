6d468759b1a5d209d1ab737fad19eedce55b9cd7
Abiel Zulio M <7030944+abielzulio@users.noreply.github.com>
2024-04-13 14:31:49 +0700

    fix

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,9 +1,19 @@
-import { Hono } from 'hono'
+import { Hono } from "hono"
+import { trpcServer } from "@hono/trpc-server" // Deno 'npm:@hono/trpc-server'
+import { appRouter } from "./router"
+import { showRoutes } from "hono/dev"
 
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.use(
+  "/trpc/*",
+  trpcServer({
+    router: appRouter,
+  })
+)
+
+showRoutes(app)
 
 export default app
+
+export type AppRouter = typeof appRouter

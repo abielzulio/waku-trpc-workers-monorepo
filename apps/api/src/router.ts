6ccc6ff30da91c9cdaab40c6366bf2be8550e24f
Abiel Zulio M <7030944+abielzulio@users.noreply.github.com>
2024-04-13 14:31:38 +0700

    add router

---
+++ apps/api/src/router.ts
@@ -0,0 +1,13 @@
+import { initTRPC } from "@trpc/server"
+import { z } from "zod"
+
+const t = initTRPC.create()
+
+const publicProcedure = t.procedure
+const router = t.router
+
+export const appRouter = router({
+  hello: publicProcedure.input(z.string().nullish()).query(({ input }) => {
+    return `Hello ${input ?? "World"}!`
+  }),
+})
